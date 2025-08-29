045a5ced9acd2dc70bf663109d49fdff9936c8f7
aylenfrancou <francouaylen2@gmail.com>
2025-08-29 18:59:46 +0000

    Avance TP1

---
+++ TP1/Punto3.java
@@ -0,0 +1,13 @@
+public class Punto3 {
+    public static void main(String[] args) {
+        new int A, B, C, Aux;
+        A=1;
+        B=2;
+        C=3;
+        Aux=A;
+        A=C;
+        B=Aux;
+        C=Aux;
+
+    }
+}
---
+++ TP1/Punto4.java
@@ -0,0 +1,6 @@
+public class Punto4 {
+    public static void main(String[] args) {
+        int NRO1 = Console.readInt()
+        int NRO2 = Console.readInt()
+        int NRO3 = Console.readInt()
+}
